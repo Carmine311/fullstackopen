7ecc3e596b7c9d831616e436a447f62af8c652ae
Carmine Sampogna <carmine.sampogna311@gmail.com>
2021-12-07 22:22:28 -0500

    State1

--- part1/basics/src/App.js
+++ part1/basics/src/App.js
@@ -1,37 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Hello = ({name, age}) => {
-  
-  const bornYear = () => new Date().getFullYear() - age;
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Footer = () => {
-  return(
-    <div>
-      greeting app created by Carmine
-    </div>
-  )
+const Button = ({handleClick, text}) => {
+  return <button onClick={handleClick} >
+    {text}
+  </button>
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  console.log('rendering...', counter)
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Carmine" age={26 + 10} />
-      <Hello name="Frank" age={age} />
-      <Footer />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text='Increase' />
+      <Button handleClick={decreaseByOne} text='Decrease' />
+      <Button handleClick={setToZero} text='Reset' />
+    </div>
   )
 }
 
