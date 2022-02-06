4d62ca43eb7cf4608b40e5a5b2638807fd3f71e8
Carmine Sampogna <carmine.sampogna311@gmail.com>
2022-02-06 18:04:55 -0500

    Added code to toggle importance and update notes

--- part2/collections/src/App.js
+++ part2/collections/src/App.js
@@ -1,68 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Note from './components/Note'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
+import NoteList from "./components/NoteList";
 
 const App = (props) => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('...a new note')
-  const [showAll, setShowAll] = useState(true)
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("...a new note");
+  const [showAll, setShowAll] = useState(true);
 
   const hook = () => {
-    console.log('effect')
-    axios.get('http://localhost:3005/notes')
-    .then(response => {
-      console.log('Promise fulfilled')
-      setNotes(response.data)
-    })
-  }
+    console.log("effect");
+    axios.get("http://localhost:3005/notes").then((response) => {
+      console.log("Promise fulfilled");
+      setNotes(response.data);
+    });
+  };
 
-  useEffect(hook, [])
-
-  console.log('Render ', notes.length, ' notes')
+  useEffect(hook, []);
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    }
+    };
 
-    axios.post('http://localhost:3005/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
-  }
+    axios.post("http://localhost:3005/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
+  };
 
   const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
+    setNewNote(event.target.value);
+  };
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important)
+  const toggleImportance = (id) => {
+    const url = `http://localhost:3005/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    axios.put(url, changedNote)
+    .then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
-        <button onClick={() => setShowAll(!showAll)} >
-          show {showAll ? 'important' : 'all'}
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
         </button>
       </div>
-      <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <NoteList notes={notesToShow} toggleImportance={toggleImportance} />
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part2/collections/src/components/NoteList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Note from "./Note";
+
+const NoteList = ({ notes, toggleImportance }) => {
+
+  
+
+  return (
+    <ul>
+      {notes.map((note) => (
+        <Note key={note.id} note={note} toggleImportance={toggleImportance} />
+      ))}
+    </ul>
+  );
+};
+
+export default NoteList;
